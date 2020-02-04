48ee76ef2aa194110710daac6f31767021c87e8d
Kuralamuthan Kathirvelan <kuralamuthan300@gmail.com>
2020-02-04 21:41:07 +0530

    Data acquired and sent to acknowlwdge.php

---
+++ Register/Acknowledgement/acknowledge.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Name    : <?php echo $_POST["first_name"]; echo ' '; echo $_POST["last_name"]; ?><br/>
+Email   : <?php echo $_POST["email"];?><br/>
+Phone   :<?php echo $_POST["phone"];?><br/>
+College ID   :<?php echo $_POST["c_id"];?><br/>
+College : <?php echo $_POST["c_name"];?><br/>
+College city :<?php echo $_POST["college_city"];?><br/>
+
+
+
+</body>
+</html>
